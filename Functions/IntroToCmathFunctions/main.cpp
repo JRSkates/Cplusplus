3bebfdb5da2f444f1bed3310c9792fc926707358
JRSkates <skatesproduction@gmail.com>
2024-11-28 12:30:59 +0000

    cmath challenge

--- Functions/IntroToCmathFunctions/main.cpp
+++ Functions/IntroToCmathFunctions/main.cpp
@@ -37,11 +37,14 @@ int main() {
     double result = pow(2.0, input);
     cout << "2 raised to the power of " << input << " is: " << result << endl;
 
-    cout << "Integer square root of " << input << " (using the sqrt function from the cmath library): " << sqrt(9.0) << endl;
+    result = sqrt(input);
+    cout << "Integer square root of " << input << " (using the sqrt function from the cmath library): " << result << endl;
     
-    cout << "Integer square root (using the floor function from the cmath library): " << floor(sqrt(9.0)) << endl;
+    result = floor(sqrt(input)); // This will round down to the nearest integer
+    cout << "Integer square root (using the floor function from the cmath library): " << result << endl;
 
-    cout << "Remainder of " << input << " divided by 3 (using the fmod function from the cmath library): " << fmod(input, 3) << endl;
+    result = fmod(input, 3); // This will return the remainder of the division, so it will be 0
+    cout << "Remainder of " << input << " divided by 3 (using the fmod function from the cmath library): " << result << endl;
 
     cout << "----------------------------------------------------------------" << endl;
 
@@ -52,17 +55,35 @@ int main() {
 
     // seed the random number generator
     // if you don't seed the generator you will get the same requence random numbers every run
-    // seeding with nullptr, which is just 0
+    // seeding with the current time, using a nullptr which is just 0
+    // this is the "current time" as a value, meaning it will change every run
     cout << "RAND_MAX on my system is: " << RAND_MAX << endl;
     srand(time(nullptr));
 
     // create a loop to generate a random number in the range
     // up to the count amount 
     // ie: 10 random numbers, each between 1 and 6
+    cout << "Generating " << count << " random numbers between " << min << " and " << max << endl;
+
     for (size_t i {1}; i <= count; i++) {
         random_number = rand() % max + min;
         cout << "Random number: " << random_number << endl;
     }
+
+    cout << "----------------------------------------------------------------" << endl;
+
+    double bill_total {102.78};
+    int number_of_guests {5};
+    
+    double individual_bill = bill_total / number_of_guests;
+    
+    double individual_bill_1 = floor(individual_bill);
+    double individual_bill_2 = round(individual_bill);
+    double individual_bill_3 = ceil((individual_bill * 100)) / 100;
+    
+    cout << "The individual bill at location 1 will be $" << individual_bill_1 
+        << "\n" << "The individual bill at location 2 will be $" << individual_bill_2 
+        << "\n" << "The individual bill at location 3 will be $" << individual_bill_3;
 
     cout << endl;
     cout << "================================================================" << endl;
