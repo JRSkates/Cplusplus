2f10563ba5db2a419b7fbc1d3a22ad41a5b81905
JRSkates <skatesproduction@gmail.com>
2024-12-04 14:33:04 +0000

    Copy Constructor

---
+++ ClassesAndObjects/CopyConstructor/Player.cpp
@@ -0,0 +1,51 @@
+#include "Player.h"
+
+// Three-arg constructor
+// Default constructor parameters added in Player.h
+Player::Player(std::string name_val , int health_val, int xp_val)
+    : name{name_val}, health{health_val}, xp{xp_val} {
+    cout << "Three-args constructor called for " << name << endl;
+}
+
+// No-args constructor
+// Player::Player() : Player {"None", 0, 0} {
+//     cout << "No-args constructor called for " << name << endl;
+// }
+
+// // One-arg constructor
+// Player::Player(string name_val) : Player {name_val, 0, 0} {
+//     cout << "One-arg constructor called for " << name << endl;
+// }
+
+
+// Destructor
+Player::~Player() {
+    cout << "Destructor called for " << name << endl;
+}
+
+// Getter method
+string Player::get_name() {
+    return name;
+}
+
+void Player::set_name(string name_val) { name = name_val; };
+
+int Player::get_health() { return health; };
+
+int Player::get_xp() { return xp; };
+
+void Player::display_player() {
+    cout << "Player name is: " << name << endl;
+    cout << "Health is: " << health << endl;
+    cout << "XP is: " << xp << endl;
+}
+
+// We pass by reference the object of the same class we want to copy
+// the original class object is passed as a const
+// So it is not modified
+Player::Player(const Player &source) 
+    : name{source.name},
+      health{source.health}, 
+      xp{source.xp} {
+    cout << "Copy constructor called for " << name << endl;
+};
---
+++ ClassesAndObjects/CopyConstructor/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include "Player.h"
+
+using namespace std;
+// Copy Constructor
+// When objects are copied C++ must create a new object from an existing object
+// When is a copy of an object made:
+// - passing object by value as a parameter
+// - returning an object from a function by value
+// - constructing one object based on another of the same class
+// C++ must have a way of accomplishing this 
+// so it provides a complier-defined copy constructor if you don't
+
+
+int main() {
+    cout << "=============================================================" << endl;
+    cout << endl;
+
+
+    Player hero("Hero", 200, 100);
+    cout << "Player name of hero is: " << hero.get_name() << endl;
+    cout << "Health is: " << hero.get_health() << endl;
+    cout << "XP is: " << hero.get_xp() << endl;
+
+    cout << endl;
+
+    
+    // Copy constructor example
+    Player hero_copy(hero);
+    cout << endl;
+    // (const &hero);
+    hero_copy.display_player();
+
+    // This creates a true copy, as its passed by reference, not by value
+
+    
+    cout << endl;
+    cout << "=============================================================" << endl;
+
+    return 0;
+}
+
+// Need to complie this manually with:
+// clang++ -std=c++17 main.cpp Player.cpp -o main && ./main
+// We can change the task.json file to allow this in a specific project
+// But not for the general usage we have here
